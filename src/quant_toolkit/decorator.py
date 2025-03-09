733f00c5425c00765efd110ab5f2ea79cbec27cd
cheesecakeMafia <f2013359@gmail.com>
2025-03-09 22:37:10 +0530

    major changes incoming

--- src/quant_toolkit/decorator.py
+++ src/quant_toolkit/decorator.py
@@ -6,7 +6,7 @@ A thing to remeber is that it will make the function call on all cases, even whe
 import time
 from functools import wraps
 import inspect
-from typing import Callable, get_type_hints, Any
+from typing import Callable, Union, get_type_hints, Any
 import datetime
 import logging
 
@@ -140,7 +140,7 @@ def memoize(func: Callable) -> Callable:
 
 
 class logit:
-    """A class to log the function call"""
+    """A class to log the function call. This doesn't handle error logging and thus we need another function to do that for us."""
 
     def __init__(
         self,
@@ -168,6 +168,82 @@ class logit:
         logging.basicConfig(filename=self.path, level=self.loggin_level)
         logging.debug(message)
         return result
+
+
+class MyLogger:
+    def __init__(self):
+        logging.basicConfig(level=logging.DEBUG)
+
+    def get_logger(self, name=None):
+        return logging.getLogger(name)
+
+
+def get_default_logger():
+    return MyLogger().get_logger()
+
+
+def log(_func=None, *, my_logger: Union[MyLogger, logging.Logger] = None):
+    def decorator_log(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            logger = get_default_logger()
+            try:
+                if my_logger is None:
+                    first_args = next(
+                        iter(args), None
+                    )  # capture first arg to check for `self`
+                    logger_params = [  # does kwargs have any logger
+                        x
+                        for x in kwargs.values()
+                        if isinstance(x, logging.Logger) or isinstance(x, MyLogger)
+                    ] + [  # # does args have any logger
+                        x
+                        for x in args
+                        if isinstance(x, logging.Logger) or isinstance(x, MyLogger)
+                    ]
+                    if hasattr(first_args, "__dict__"):  # is first argument `self`
+                        logger_params = (
+                            logger_params
+                            + [
+                                x
+                                for x in first_args.__dict__.values()  # does class (dict) members have any logger
+                                if isinstance(x, logging.Logger)
+                                or isinstance(x, MyLogger)
+                            ]
+                        )
+                    h_logger = next(
+                        iter(logger_params), MyLogger()
+                    )  # get the next/first/default logger
+                else:
+                    h_logger = my_logger  # logger is passed explicitly to the decorator
+
+                if isinstance(h_logger, MyLogger):
+                    logger = h_logger.get_logger(func.__name__)
+                else:
+                    logger = h_logger
+
+                args_repr = [repr(a) for a in args]
+                kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]
+                signature = ", ".join(args_repr + kwargs_repr)
+                logger.debug(f"function {func.__name__} called with args {signature}")
+            except Exception:
+                pass
+
+            try:
+                result = func(*args, **kwargs)
+                return result
+            except Exception as e:
+                logger.exception(
+                    f"Exception raised in {func.__name__}. exception: {str(e)}"
+                )
+                raise e
+
+        return wrapper
+
+    if _func is None:
+        return decorator_log
+    else:
+        return decorator_log(_func)
 
 
 """Here are some great examples of decorators found on GitHub:
